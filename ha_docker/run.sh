1d08ea0f0e85764306f59703c48197e2a1011262
Ruslan <58290307+kanteruk@users.noreply.github.com>
2025-12-04 01:43:52 +0200

    Update run.sh

--- ha_docker/run.sh
+++ ha_docker/run.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
 echo "RF Addon started (Ubuntu 22.04 + Python)"
 echo "Python version:"
 python3 --version
 
-set -e
-
 # Вказуємо порт FastAPI з конфігурації аддону або дефолт
-PORT="${PORT:-8000}"
+PORT="${PORT:-8080}"
 
 # Запуск FastAPI
 exec uvicorn main:app --host 0.0.0.0 --port $PORT
