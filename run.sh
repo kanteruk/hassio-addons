69bc5d30d3a8edcf44d4463f86e7b65253c4aa3f
Ruslan <58290307+kanteruk@users.noreply.github.com>
2025-12-03 23:54:58 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "RF Addon started (Ubuntu 22.04 + Python)"
-echo "Python version:"
-python3 --version
-
-# ТУТ запускай свій сервіс
-# Приклад: Python HTTP server
-python3 -m http.server 8080
